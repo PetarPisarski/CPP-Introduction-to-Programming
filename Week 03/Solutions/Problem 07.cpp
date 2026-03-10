4ecdf72b330534ec23e84a8c37d19afdb8daff68
PetarPisarski <peter.pisarski@outlook.com>
2026-03-10 15:03:33 -0700

    First Week Commit

--- Week 03/Solutions/Problem 07.cpp
+++ Week 03/Solutions/Problem 07.cpp
@@ -3,7 +3,7 @@
 
 using std::cin, std::cout, std::endl;
 
-const double EPSILON    =   1e-9;
+const double EPSILON    =   1E-9;
 
 void solveEquation1();
 void solveEquation2();
@@ -11,23 +11,23 @@ void solveEquation3();
 
 
 int main() {
-
+    solveEquation1();
 
 
     return 0;
 }
 
-void solveLinearEquation1() {
+void solveEquation1() {
     double  a   =   0.0;
     double  b   =   0.0;
 
     puts("Enter coefficient A: ");
     cin >> a;
-    puts("Enter coefficient A: ");
+    puts("Enter coefficient B: ");
     cin >> b;
 
-    unsigned short int  bit1    =   (fabs(a) >= EPSILON);
-    unsigned short int  bit2    =   (fabs(b) >= EPSILON);
+    unsigned short int  bit1 = (fabs(a) >= EPSILON);
+    unsigned short int  bit2 = (fabs(b) >= EPSILON);
 
     unsigned short int  number  =   0;
 
@@ -35,16 +35,16 @@ void solveLinearEquation1() {
     number  =   ((number & ~(1 << 1)) | (bit2 << 1));
 
 
-    // 1 Case: A == 0, B == 0 -> x ∈ R
-    // 2 Case: A != 0, B == 0 -> x = 0
-    // 3 Case: A == 0, B != 0 -> x ∈ ∅
-    // 4 Case: A != 0, B != 0 -> x = -B / A
+    // 1 Case: A == 0, B == 0 -> x ∈ R          0000
+    // 2 Case: A != 0, B == 0 -> x = 0          0001
+    // 3 Case: A == 0, B != 0 -> x ∈ ∅          0010
+    // 4 Case: A != 0, B != 0 -> x = -B / A     0011
 
     switch (number) {
-        case 0:     printf("Every real number is a solution!\n");   break;
-        case 1:     printf("Solution is: X = %lf\n", 0.000);        break;
-        case 2:     printf("There are no solutions!\n");            break;
-        case 3:     printf("Solution is: X = %lf\n", -b / a);       break;
+        case 0:     cout << "Every real number is a solution" << endl;   break;
+        case 1:     cout << "The Solution is X = " << 0 << endl;    break;
+        case 2:     cout << "There are no real solutions" << endl;    break;
+        case 3:     cout << "The Solution is X = " << ((-b)/a) << endl;   break;
         default:    break;
     }
 }
